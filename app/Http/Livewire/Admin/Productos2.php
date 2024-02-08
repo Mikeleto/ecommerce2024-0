0d598b28f67f3bee63f9aa306de89353db20aec2
Mikeleto <miguel2015mlsol13@gmail.com>
2024-02-08 15:52:21 +0100

    Tarea final 2 segunda parte

--- app/Http/Livewire/Admin/Productos2.php
+++ app/Http/Livewire/Admin/Productos2.php
@@ -3,6 +3,13 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\Product;
+use App\Models\Category;
+use App\Models\Subcategory;
+use App\Models\ColorProduct;
+use App\Models\Color;
+use App\Models\ColorSize;
+use App\Models\Size;
+use App\Models\Brand;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -11,17 +18,122 @@ class Productos2 extends Component
     use WithPagination;
 
     public $search;
+    public $perPage = 10; 
+    public $name = true;
+    public $category = true;
+    public $status = true;
+    public $price = true;
+    public $subcategory = true;
+    public $brand = true;
+    public $created_at = true;
+    public $colors = true;
+    public $stockColor = true; 
+    public $sizes = true;
+    public $stockSizes = true;
+    public $stock = true;
+
+    public $sortField = 'name';
+    public $sortBrandField = 'name';
+    public $sortDirection = 'asc';
+
+    public function sortBy($field){
+        if($this->sortField === $field){
+            $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+        }else{
+            if($field === 'subcategory.category'){
+                $this->sortField = 'subcategory_id' ;
+            } elseif($field === 'brands'){
+                $this->sortField = 'name' ;
+            }else{
+                $this->sortField = $field;
+            }
+        }
+    }
+
+    public function sortSubcategory($field){
+        if($this->sortField === $field){
+            $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+            
+        }else{
+            if($field === 'name'){
+                $this->sortField = 'name' ;
+            } 
+        }
+    }
+
+    public function sortBrand($field){
+        if($this->sortBrandField === $field){
+            $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+        }
+    }
+
+    public function sortColor($field){
+        if($this->sortField === $field){
+            $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+        }
+    }
+
+    public function sortSize($field){
+        if($this->sortField === $field){
+            $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+        }
+    }
+
+    
+
+  
 
     public function updatingSearch()
     {
         $this->resetPage();
     }
 
+     
+
     public function render()
     {
-        $products = Product::where('name', 'LIKE', "%{$this->search}%")->paginate(10);
+        $products = Product::where('name', 'LIKE', "%{$this->search}%")
+        ->orderBy($this->sortField, $this->sortDirection)
+        
+    ->paginate($this->perPage);
 
-        return view('livewire.admin.productos2', compact('products'))
-            ->layout('layouts.admin');
+    $subcategories = Subcategory::where('name', 'LIKE', "%{$this->search}%")
+    ->orderBy($this->sortField, $this->sortDirection);
+
+    $brands = Brand::where('name', 'LIKE', "%{$this->search}%")
+        ->orderBy($this->sortBrandField, $this->sortDirection);
+
+        
+    
+        
+    
+        $colorProduct = ColorProduct::all();
+        $color = Color::all();
+        $colorSize = ColorSize::all();
+        $size = Size::all();
+    
+        return view('livewire.admin.productos2', [
+
+            'products' => $products,
+            'subcategories' => $subcategories,
+            'brands' => $brands,
+            'colorProduct' => $colorProduct,
+            'color' => $color,
+            'colorSize' => $colorSize,
+            'size' => $size,
+            'search' => $this->search, 
+            'name' => $this->name,
+            'category' => $this->category,
+            'status' => $this->status,
+            'price' => $this->price,
+            'subcategory' => $this->subcategory,
+            'brand' => $this->brand,
+            'created_at' => $this->created_at,
+            'colors' => $this->colors, 
+            'stockColor' => $this->stockColor,
+            'sizes' => $this->sizes, 
+            'stockSizes' => $this->stockSizes,
+            'stock' => $this->stock, 
+        ])->layout('layouts.admin');
     }
 }
--- resources/views/livewire/admin/productos2.blade.php
+++ resources/views/livewire/admin/productos2.blade.php
@@ -10,8 +10,53 @@
             </x-button-link>
         </div>
     </x-slot>
+  
 
     <x-table-responsive>
+    <div class="flex inline-flex border-gray-300 border mx-6 p-8">
+            <x-jet-dropdown>
+                <x-slot name="trigger">
+                    <button>Columnas a mostrar</button>
+                </x-slot>
+
+                <x-slot name="content">
+                    <div>
+                        Nombre<input type="checkbox" wire:model="name">
+                    </div>
+                    <div>
+                        Categoria<input type="checkbox" wire:model="category">
+                    </div>
+                    <div>
+                        Estado<input type="checkbox" wire:model="status">
+                    </div>
+                    <div>
+                        Precio<input type="checkbox" wire:model="price">
+                    </div>
+                    <div>
+                        Marca<input type="checkbox" wire:model="brand">
+                    </div>
+                    <div>
+                        Fecha<input type="checkbox" wire:model="created_at">
+                    </div>
+                    <div>
+                        Color<input type="checkbox" wire:model="colors">
+                    </div>
+                    <div>
+                        Stock Color<input type="checkbox" wire:model="stockColor">
+                    </div>
+                    <div>
+                        Talla<input type="checkbox" wire:model="sizes">
+                    </div>
+                    <div>
+                        Stock talla<input type="checkbox" wire:model="stockSizes">
+                    </div>
+                    <div>
+                        Stock<input type="checkbox" wire:model="stock">
+                    </div>
+                  
+                </x-slot>
+            </x-jet-dropdown>
+        </div>
         <div class="px-6 py-4">
             <x-jet-input class="w-full"
                          wire:model="search"
@@ -23,38 +68,101 @@
             <table class="min-w-full divide-y divide-gray-200">
                 <thead class="bg-gray-50">
                 <tr>
+                    @if($name)
                     <th scope="col"
                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Nombre
+                        <a wire:click="sortBy('name')" href="#">
+                    Nombre
+                    </a>
                     </th>
+                    @endif
+                    @if($category)
                     <th scope="col"
                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Categoría
+                        <a wire:click="sortBy('subcategory_id')" href="#">
+                    Categoria
+                    </a>
                     </th>
+                    @endif
+                    @if($status)
                     <th scope="col"
                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <a wire:click="sortBy('status')" href="#">
                         Estado
                     </th>
+                    @endif
+                    @if($price)
                     <th scope="col"
                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Precio
+                        <a wire:click="sortBy('price')" href="#">
+                    Precio
+                    </a>
                     </th>
+                    @endif
+                    @if($subcategory)
                     <th scope="col"
                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Subcategorias
+                        <a wire:click="sortSubcategory('name')" href="#">
+                    Subcategoria
+                    </a>
                     </th>
+                    @endif
+                    @if($brand)
                     <th scope="col"
                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Marcas
+                        <a wire:click="sortBrand('name')" href="#">
+                    Marca
+                    </a>
                     </th>
+                    @endif
+                    @if($created_at)
                     <th scope="col"
                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Creado el 
+                        <a wire:click="sortBy('created_at')" href="#">
+                    Creado el
+                    </a>
                     </th>
+                    @endif
+                    @if($colors)
                     <th scope="col"
                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Stock 
+                        <a wire:click="sortBy('name')" href="#">
+                    Color
+                    </a>
                     </th>
+                    @endif
+                    @if($stockColor)
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <a wire:click="sortBy('quantity')" href="#">
+                    Stock Color
+                    </a>
+                    </th>
+                    @endif
+                    @if($sizes)
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <a wire:click="sortBy('name')" href="#">
+                    Talla
+                    </a>
+                    </th>
+                    @endif
+                    @if($stockSizes)
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <a wire:click="sortBy('quantity')" href="#">
+                    Stock talla
+                    </a>
+                    </th>
+                    @endif
+                    @if($stock)
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <a wire:click="sortBy('quantity')" href="#">
+                        Stock
+                        </a>
+                    </th>
+                    @endif
 
 
                     <th scope="col" class="relative px-6 py-3">
@@ -65,6 +173,7 @@
                 <tbody class="bg-white divide-y divide-gray-200">
                 @foreach($products as $product)
                     <tr>
+                        @if($name)
                         <td class="px-6 py-4 whitespace-nowrap">
                             <div class="flex items-center">
                                 <div class="flex-shrink-0 h-10 w-10 object-cover">
@@ -79,31 +188,81 @@
                                 </div>
                             </div>
                         </td>
+                        @endif
+                        @if($category)
                         <td class="px-6 py-4 whitespace-nowrap">
                             <div class="text-sm text-gray-900">{{ $product->subcategory->category->name }}</div>
                             <div class="text-sm text-gray-500">{{ $product->subcategory->name }}</div>
                         </td>
+                        @endif
+                        @if($status)
                         <td class="px-6 py-4 whitespace-nowrap">
                             <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-{{ $product->status == 1 ? 'red' : 'green'
                             }}-100 text-{{ $product->status == 1 ? 'red' : 'green' }}-800">
                                 {{ $product->status == 1 ? 'Borrador' : 'Publicado' }}
                             </span>
                         </td>
+                        @endif
+                        @if($price)
                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                             {{ $product->price }} &euro;
                         </td>
+                        @endif
+                        @if($subcategory)
                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                             {{ $product->subcategory->name }}
                         </td>
+                        @endif
+                        @if($brand)
                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                             {{ $product->brand->name }}
                         </td>
+                        @endif
+                        @if($created_at)
                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                             {{ $product->created_at }}
                         </td>
+                        @endif
+                        @if($colors)
+                        @if($product->colors->isNotEmpty())
+                        <td class="px-6 py-4 whitespace-nowrap">
+                        @foreach($color as $c)
+                        <div class="text-sm text-gray-900">{{ $c->name }}</div>
+                        @endforeach
+                        </td>
+                        @endif
+                        @if($stockColor)
+                        <td class="px-6 py-4 whitespace-nowrap">
+                        @foreach($colorProduct as $colors)
+                        <div class="text-sm text-gray-500">{{ $colors->quantity }}</div>
+                        @endforeach
+                        </td>
+                        @else
+                        @endif
+                        @endif
+                        @if($sizes)
+                        @if($product->sizes->isNotEmpty())
+                        <td class="px-6 py-4 whitespace-nowrap">
+                        @foreach($size as $s)
+                        <div class="text-sm text-gray-900">{{ $s->name }}</div>
+                        @endforeach
+                        </td>
+                        @endif
+                        @if($stockSizes)
+                     
+                        <td class="px-6 py-4 whitespace-nowrap">
+                        @foreach($colorSize as $sizes)
+                        {{ $sizes->quantity }}
+                        @endforeach
+                        </td>
+                        @endif
+                        @else
+                        @endif
+                      @if($stock)
                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                             {{ $product->quantity }}
                         </td>
+                        @endif
                         <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                             <a href="{{ route('admin.products.edit', $product) }}"
                                class="text-indigo-600 hover:text-indigo-900">Editar</a>
@@ -123,5 +282,16 @@
                 {{ $products->links() }}
             </div>
         @endif
+        <div>
+    <label for="perPage">Elementos por página:</label>
+    <select wire:model="perPage" id="perPage">
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="15">15</option>
+        <option value="20">20</option>
+        <option value="25">25</option>
+        <option value="50">50</option>
+    </select>
+</div>
     </x-table-responsive>
 </div>
